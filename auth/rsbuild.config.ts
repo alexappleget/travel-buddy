d50116ae705880799b69816030279b96cff3aa56
Alex Appleget <alexappleget2014@gmail.com>
2026-04-18 12:17:52 -0500

    created zephyr github action and testing

--- auth/rsbuild.config.ts
+++ auth/rsbuild.config.ts
@@ -3,22 +3,27 @@ import { pluginReact } from "@rsbuild/plugin-react";
 import { pluginModuleFederation } from "@module-federation/rsbuild-plugin";
 import { withZephyr } from "zephyr-rsbuild-plugin";
 
+const plugins = [
+  pluginReact(),
+  pluginModuleFederation({
+    name: "federation_auth",
+    filename: "remoteEntry.js",
+    exposes: {
+      "./components": "./src/exports.ts",
+    },
+    shared: ["react", "react-dom"],
+  }),
+];
+
+if (process.env.CI) {
+  plugins.push(withZephyr());
+}
+
 export default defineConfig({
   output: {
     assetPrefix: "auto",
   },
-  plugins: [
-    pluginReact(),
-    pluginModuleFederation({
-      name: "federation_auth",
-      filename: "remoteEntry.js",
-      exposes: {
-        "./components": "./src/exports.ts",
-      },
-      shared: ["react", "react-dom"],
-    }),
-    withZephyr(),
-  ],
+  plugins,
   server: {
     port: 3000,
   },
--- shell/rsbuild.config.ts
+++ shell/rsbuild.config.ts
@@ -3,19 +3,24 @@ import { pluginReact } from "@rsbuild/plugin-react";
 import { pluginModuleFederation } from "@module-federation/rsbuild-plugin";
 import { withZephyr } from "zephyr-rsbuild-plugin";
 
+const plugins = [
+  pluginReact(),
+  pluginModuleFederation({
+    name: "federation_shell",
+    remotes: {
+      federation_auth:
+        "federation_auth@http://localhost:3000/mf-manifest.json",
+    },
+    shared: ["react", "react-dom"],
+  }),
+];
+
+if (process.env.CI) {
+  plugins.push(withZephyr());
+}
+
 export default defineConfig({
-  plugins: [
-    pluginReact(),
-    pluginModuleFederation({
-      name: "federation_shell",
-      remotes: {
-        federation_auth:
-          "federation_auth@http://localhost:3000/mf-manifest.json",
-      },
-      shared: ["react", "react-dom"],
-    }),
-    withZephyr(),
-  ],
+  plugins,
   server: {
     port: 2000,
   },
