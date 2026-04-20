dfee718f1973535e7bfaf7e6456f1fcf3a9b5725
Alex Appleget <alexappleget2014@gmail.com>
2026-04-19 20:34:56 -0500

    asdf

--- auth/src/supabase/client.ts
+++ auth/src/supabase/client.ts
@@ -2,8 +2,8 @@ import { createBrowserClient } from "@supabase/ssr";
 
 export function createClient() {
   return createBrowserClient(
-    import.meta.env.ZE_PUBLIC_SUPABASE_URL,
-    import.meta.env.ZE_PUBLIC_SUPABASE_ANON_KEY,
+    import.meta.env.PUBLIC_SUPABASE_URL,
+    import.meta.env.PUBLIC_SUPABASE_ANON_KEY,
   );
 }
 
